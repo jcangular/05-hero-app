a4af0618c587b3acf946020fdae5b4ff0f4db56a
José Carlos Palma <palmahn@gmail.com>
2021-02-16 19:10:40 -0600

    Configuración de rutas del auth en el appRouting.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+    { path: 'oops', component: PageNotFoundComponent },
+    { path: '**', redirectTo: 'oops' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
